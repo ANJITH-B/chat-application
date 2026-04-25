d7a5c2e85e189973119c15fceadbca007ad063da
ANJITH B <anjithb200@gmail.com>
2026-04-25 11:02:03 +0530

    fix(stratergy): auth callback-url @define path

--- server/src/lib/passport.js
+++ server/src/lib/passport.js
@@ -9,7 +9,7 @@ dotenv.config();
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/api/auth/google/callback"
+    callbackURL: process.env.CLIENT_URL + "/api/auth/google/callback"
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
@@ -41,7 +41,7 @@ passport.use(new GoogleStrategy({
 passport.use(new GitHubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: "/api/auth/github/callback"
+    callbackURL: process.env.CLIENT_URL + "/api/auth/github/callback"
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
